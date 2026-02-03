217b66b173e4835453d3666dedd99a4e2dd5d96a
KacperOmen <komen1@stu.vistula.edu.pl>
2026-02-03 15:14:12 +0100

    Create Register Page + routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import HomePage from "./pages/HomePage"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import RegisterPage from "./pages/RegisterPage"
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,21 @@
 import logo from '../assets/images/logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='h-25 flex justify-between bg-gray-200 pl-5 border-b'>
-        <div className='flex items-center gap-5 cursor-pointer'>
-            <img className='h-full' src={logo}></img>
+        <Link to="/">
+          <div className='flex items-center gap-5 cursor-pointer'>
+            <img className='h-25' src={logo}></img>
             <h2 className='text-3xl lg:text-4xl hidden md:block'>Personal Habit Tracker</h2>
-        </div>   
+          </div>
+        </Link>
+           
         <div className='flex bg-gray-300'>
             <div className='text-2xl md:text-3xl lg:text-4xl hover:bg-gray-400 border-x px-5 flex flex-col justify-center transition cursor-pointer'><p>Log in</p></div>    
-            <div className='text-2xl md:text-3xl lg:text-4xl hover:bg-gray-400 border-x px-5 flex flex-col justify-center transition cursor-pointer'><p>Sign in</p></div>    
+            <Link to="register">
+              <div className='h-25 text-2xl md:text-3xl lg:text-4xl hover:bg-gray-400 border-x px-5 flex flex-col justify-center transition cursor-pointer'><p>Sign in</p></div>
+            </Link>  
         </div> 
     </div>
   )
