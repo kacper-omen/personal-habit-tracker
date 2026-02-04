e094dba35983d9271651f78f4d7cdf599a224b5f
KacperOmen <komen1@stu.vistula.edu.pl>
2026-02-04 13:33:11 +0100

    Add register form submit handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import HomePage from "./pages/HomePage"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage"
+import {ToastContainer} from 'react-toastify'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
