3ba266e34bca581cb6dccb29b889956127f9cf38
KacperOmen <komen1@stu.vistula.edu.pl>
2026-02-04 13:56:19 +0100

    Delete Header and Footer components from HomePage.jsx file

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,9 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
 import Hero from "../components/Hero"
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
       <Hero />
-      <Footer />
     </div>
   )
 }
