d40d6c04cae221339d2a1cd74d81e0c171fb4be0
KacperOmen <komen1@stu.vistula.edu.pl>
2026-02-05 15:46:04 +0100

    Fetch data from backend and create upper section of SingleHabitPage

--- frontend/src/pages/SingleHabitPage.jsx
+++ frontend/src/pages/SingleHabitPage.jsx
@@ -1,8 +1,44 @@
+import { IoGameController, IoEllipsisHorizontalCircleSharp  } from "react-icons/io5";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SingleHabitPage = () => {
+  const [habit, setHabit] = useState({
+    name: "",
+    description: "",
+    category: "",
+    frequency: "",
+    daysOfWeek: "",
+  })
+
+  const {id} = useParams()
+
+  useEffect(() => { 
+    const fetchHabit = async () => {
+        try {
+            const {data} = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/habits/${id}`)
+            setHabit(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    fetchHabit()
+  }, [])
+
   return (
     <div>
-        
+        <div className="flex items-center justify-between mx-5">
+            <div className="flex items-center justify-center gap-3">
+                <Link to="/habits">
+                    <FaArrowAltCircleLeft className="bg-white hover:text-yellow-600 cursor-pointer transition text-yellow-500 text-5xl rounded-2xl py-1 px-1" />
+                </Link>              
+                <p className="text-3xl font-bold">{habit.name}</p>
+            </div>
+            <IoGameController />
+        </div>
     </div>
   )
 }
