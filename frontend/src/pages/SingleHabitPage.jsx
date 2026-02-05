9f061a24e5ca77d6b1033c49f8370909cd6ca4fd
KacperOmen <komen1@stu.vistula.edu.pl>
2026-02-05 15:55:19 +0100

    Copy renderIcon function from HabitsPage to generate an icon based on habit category

--- frontend/src/pages/SingleHabitPage.jsx
+++ frontend/src/pages/SingleHabitPage.jsx
@@ -3,6 +3,8 @@ import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { GiHealthNormal } from "react-icons/gi";
+import { MdOutlineSportsHandball } from "react-icons/md";
 
 const SingleHabitPage = () => {
   const [habit, setHabit] = useState({
@@ -28,16 +30,31 @@ const SingleHabitPage = () => {
     fetchHabit()
   }, [])
 
+  const renderIcon = () => {
+      switch (habit.category) {
+          case "Sport":
+              return <MdOutlineSportsHandball className="text-white bg-blue-400 text-7xl rounded-xl py-2" />
+          case "Health":
+              return <GiHealthNormal className="text-white bg-red-600 text-7xl rounded-xl py-2" />
+          case "Entertainment":
+              return <IoGameController className="text-white bg-violet-900 text-7xl rounded-xl py-2" />
+          case "Other":
+              return <IoEllipsisHorizontalCircleSharp className="text-white bg-black text-7xl rounded-xl py-2" />
+          default:
+              return null
+      }
+  }
+
   return (
     <div>
-        <div className="flex items-center justify-between mx-5">
+        <div className="flex items-center justify-between mx-5 my-5">
             <div className="flex items-center justify-center gap-3">
                 <Link to="/habits">
                     <FaArrowAltCircleLeft className="bg-white hover:text-yellow-600 cursor-pointer transition text-yellow-500 text-5xl rounded-2xl py-1 px-1" />
                 </Link>              
                 <p className="text-3xl font-bold">{habit.name}</p>
             </div>
-            <IoGameController />
+            {renderIcon()}
         </div>
     </div>
   )
