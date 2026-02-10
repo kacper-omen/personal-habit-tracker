3bc0a3917e02823420a743eb870a297b3fbd4abc
KacperOmen <komen1@stu.vistula.edu.pl>
2026-02-10 13:12:00 +0100

    Create auth middleware in backend to block access to endpoints for unlogged users

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies?.token
+
+    if (!token) {
+        return res.status(401).json({message: "Access denied - User must be logged in"})
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, {}, async (error, user) => {
+        if (error) {
+            return res.status(401).json({message: "Invalid token"})
+        }
+
+        req.user = user
+        next()
+    })
+}
+
+export default requireAuth
