e7585d2135cabc41bf5f99e751f5c8b2fcc7f1b5
KacperOmen <komen1@stu.vistula.edu.pl>
2026-04-30 14:57:04 +0200

    Update markHabitAsCompleted function in habitCompletion controller to handle frequency history

--- backend/controllers/habitCompletion.controller.js
+++ backend/controllers/habitCompletion.controller.js
@@ -14,18 +14,20 @@ const markHabitAsCompleted = async (req, res) => {
         const completionDate = new Date(date)
         completionDate.setHours(0, 0, 0, 0)
 
-        if (habit.frequency !== "once" && completionDate < habit.startDay) {
+        if (habit.frequencyChangesHistory[0].frequency !== "once" && completionDate < habit.startDay) {
             return res.status(400).json({message: "Can't complete a habit before start date"})
         }
 
-        if (habit.frequency === 'weekly') {
+        const element = habit.frequencyChangesHistory.findLast(el => new Date(completionDate).getTime() >= new Date(el.from).getTime())
+
+        if (element && element.frequency === 'weekly') {
             const weekday = completionDate.toLocaleString("en-us", {weekday: "short"})
-            if (!habit.daysOfWeek.includes(weekday)) {
-                return res.status(400).json({message: `This habit is weekly and can be completed only at ${habit.daysOfWeek}`})
-            }
+            if (!element.daysOfWeek.includes(weekday)) {
+                return res.status(400).json({message: `This habit is weekly and can be completed only at ${element.daysOfWeek}`})
+            }  
         }
 
-        if (habit.frequency === "once" && !habit.listOfDays.some(date => date.getTime() === completionDate.getTime())) {
+        if (habit.frequencyChangesHistory[0].frequency === "once" && !habit.listOfDays.some(date => date.getTime() === completionDate.getTime())) {
             return res.status(400).json({message: "This task cannot be completed that date"})
         }
 
