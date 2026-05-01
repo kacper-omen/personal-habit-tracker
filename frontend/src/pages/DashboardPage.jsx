f43d3aad83edfe92ca55897bbee55528c6a508aa
KacperOmen <komen1@stu.vistula.edu.pl>
2026-05-01 14:04:54 +0200

    Update Dashboard page to handle new frequency

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -67,16 +67,16 @@ const DashboardPage = () => {
 
   useEffect(() => {
     const visible = habits.filter(habit => {
-        if (habit.frequency !== "once" && chosenDate < new Date(habit.startDay)) {
+        if (habit.frequencyChangesHistory[0].frequency !== "once" && chosenDate < new Date(habit.startDay)) {
             return false
         }
-        if (habit.frequency === "daily") {
+        if (habit.frequencyChangesHistory.at(-1).frequency === "daily") {
             return true
         }
-        else if (habit.frequency === "weekly") {
-            return habit.daysOfWeek.includes(chosenDate.toLocaleDateString("en-us", {weekday: "short"}))
+        else if (habit.frequencyChangesHistory.at(-1).frequency === "weekly") {
+            return habit.frequencyChangesHistory.at(-1).daysOfWeek.includes(chosenDate.toLocaleDateString("en-us", {weekday: "short"}))
         }
-        else if (habit.frequency === "once") {
+        else if (habit.frequencyChangesHistory[0].frequency === "once") {
             return habit.listOfDays.some(date => new Date(date).toLocaleDateString("en-us") === chosenDate.toLocaleDateString("en-us"))
         }
     })
@@ -219,7 +219,7 @@ const DashboardPage = () => {
                                 {renderIcon(visibleHabit)}
                                 <div className='flex flex-col gap-2 justify-between'>
                                     <p className='text-2xl font-bold text-slate-200 wrap-anywhere hyphens-auto'>{visibleHabit.name}</p>
-                                    {visibleHabit.frequency === "once" ? <p className='text-2xl bg-violet-600 text-white rounded-lg py-1 px-3 self-start'>Task</p> : <p className='text-2xl bg-slate-800/70 text-white rounded-lg py-1 px-3 self-start'>Habit</p>}
+                                    {visibleHabit.frequencyChangesHistory[0].frequency === "once" ? <p className='text-2xl bg-violet-600 text-white rounded-lg py-1 px-3 self-start'>Task</p> : <p className='text-2xl bg-slate-800/70 text-white rounded-lg py-1 px-3 self-start'>Habit</p>}
                                 </div>
                             </div>
                             <div>
