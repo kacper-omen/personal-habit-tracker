178f0a24067bbbae296ad304e93c9b2d105e20f0
KacperOmen <komen1@stu.vistula.edu.pl>
2026-05-20 15:31:01 +0200

    Fix bug at calculating current streak for daily frequency

--- backend/controllers/habit.controller.js
+++ backend/controllers/habit.controller.js
@@ -450,6 +450,10 @@ const getHabitStats = async (req, res) => {
                 if (filteredHabitComDesc(i).length === 1 && (filteredHabitComDesc(i)[0].date.getTime() === today.getTime() || filteredHabitComDesc(i)[0].date.getTime() === today.getTime() - (1000 * 60 * 60 * 24))) {
                     currentStreak = 1
                 }
+                if (filteredHabitComDesc(i).at(-1).date.getTime() !== frequencyDesc[i].from.getTime()) {
+                    break
+                }
+                
             }
             
             // WEEKLY
