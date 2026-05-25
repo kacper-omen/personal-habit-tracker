937735cc8397bed2a5f8328ec9409f34611aeaa8
KacperOmen <komen1@stu.vistula.edu.pl>
2026-05-25 13:19:44 +0200

    Update AuthContext import in main.jsx to match file casing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContextProvider from './context/authContext.jsx'
+import AuthContextProvider from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
