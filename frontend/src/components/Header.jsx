8a8ce1b772e7c388b85b53c0d7b3780a182f58f4
KacperOmen <komen1@stu.vistula.edu.pl>
2026-05-25 13:23:44 +0200

    Update AuthContext import in every file to match file casing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import logo from '../assets/images/logo.png'
 import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
-import { AuthContext } from '../context/authContext'
+import { AuthContext } from '../context/AuthContext'
 import { toast } from 'react-toastify'
 
 const Header = () => {
