860b0b84e195ea0bc1466d2348c6e985d95a3bd5
KacperOmen <komen1@stu.vistula.edu.pl>
2026-05-30 16:27:10 +0200

    Try to fix

--- frontend/src/pages/CreateHabitPage.jsx
+++ frontend/src/pages/CreateHabitPage.jsx
@@ -29,7 +29,11 @@ const CreateHabitPage = () => {
       axios.defaults.withCredentials = true
       const data = {name, description, category, frequency}
       if (frequency === "once") {
-        data.listOfDays = listOfDays.map(day => new Date(day).toISOString().split("T")[0])
+        data.listOfDays = listOfDays.map(day => {
+          const d = new Date(day)
+          d.setHours(0, 0, 0, 0)
+          return d
+        })
       }
       else if (frequency === "weekly") {
         data.startDay = startDate.toISOString().split("T")[0]
